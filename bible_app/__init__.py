7e33449f4c838b4a18359b5d3661cf8c425219a1
vip30service123 <thaingominh2652000@gmail.com>
2023-11-23 23:28:47 +0700

    remove main.py, use __init__.py instead, move README.md outside bible_app folder

--- bible_app/__init__.py
+++ bible_app/__init__.py
@@ -4,34 +4,6 @@ from flask import Flask, render_template, request
 import yaml
 
 
-# from bible_text_generation_web_app.src.generate import TextGenerationFactory
-
-# with open('configuration.yaml', 'r') as f:
-#     configs = yaml.safe_load(f)
-
-
-# text_generation = TextGenerationFactory(**configs)
-
-
-# app = Flask(__name__)
-
-# @app.route("/", methods = ['POST', 'GET'])
-# def generate_text(name="John"):
-
-#     generated_text = ''
-
-#     if request.method == 'POST':
-#         input_text = request.form['input']
-#         generated_text = text_generation.generate(input_text).replace('\n', '</br>')
-    
-
-#     return render_template('home.html', name=name, generated_text=generated_text)
-
-
-
-
-
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
